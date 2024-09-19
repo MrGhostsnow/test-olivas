28f435ab78be43999e2dbce37e5d01cc55745ae4
MrGhostsnow <96500538+MrGhostsnow@users.noreply.github.com>
2024-09-19 16:35:34 -0300

    feat: ContactForm

--- src/app/components/ContactForm/index.tsx
+++ src/app/components/ContactForm/index.tsx
@@ -2,10 +2,19 @@
 import { useState } from "react";
 import {
   Container,
+  ContainerContent,
+  TitleForm,
+  FormBlock,
+  FormBlockMessage,
   Form,
+  FormGrid,
+  FormInput,
+  Label,
   Input,
+  Option,
   TextArea,
   Button,
+  Checkbox,
   SuccessMessage,
 } from "./styles";
 
@@ -13,6 +22,8 @@ const ContactForm = () => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
+    phone: "",
+    function: "",
     message: "",
   });
 
@@ -35,36 +46,88 @@ const ContactForm = () => {
 
   return (
     <Container>
-      <h2>Contato</h2>
-      <Form onSubmit={handleSubmit}>
-        <Input
-          type="text"
-          name="name"
-          placeholder="Nome"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-        <Input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-        <TextArea
-          name="message"
-          placeholder="Sua mensagem"
-          value={formData.message}
-          onChange={handleChange}
-          required
-        />
-        <Button type="submit">Enviar</Button>
-      </Form>
-      {success && (
-        <SuccessMessage>Mensagem enviada com sucesso!</SuccessMessage>
-      )}
+      <ContainerContent>
+        <TitleForm>
+          <h2>
+            Entre em contato e{" "}
+            <span style={{ color: "#C6B92D" }}>receba atendimento</span>
+          </h2>
+          <p>
+            Por favor, preencha o formulário abaixo com suas informações e a sua
+            mensagem. Nossa equipe entrará em contato com você o mais breve
+            possível.
+          </p>
+        </TitleForm>
+        <Form onSubmit={handleSubmit}>
+          <FormGrid>
+            <FormBlock>
+              <Label htmlFor="name">
+                Nome completo <span style={{ color: "#C6B92D" }}>*</span>
+              </Label>
+              <Input
+                type="text"
+                name="name"
+                value={formData.name}
+                onChange={handleChange}
+                required
+              />
+            </FormBlock>
+            <FormBlock>
+              <Label htmlFor="email">
+                Email Corporativo <span style={{ color: "#C6B92D" }}>*</span>
+              </Label>
+              <Input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </FormBlock>
+            <FormBlock>
+              <Label htmlFor="phone">
+                Telefone <span style={{ color: "#C6B92D" }}>*</span>
+              </Label>
+              <Input
+                type="number"
+                name="phoneer"
+                value={formData.phone}
+                onChange={handleChange}
+                required
+              />
+            </FormBlock>
+            <FormBlock>
+              <Label htmlFor="job">
+                Qual a sua função na empresa?{" "}
+                <span style={{ color: "#C6B92D" }}>*</span>
+              </Label>
+              <Option name="job" value={formData.function} required />
+            </FormBlock>
+          </FormGrid>
+          <FormBlockMessage style={{ width: "870px" }}>
+            <Label htmlFor="message">
+              Mensagem <span style={{ color: "#C6B92D" }}>*</span>
+            </Label>
+            <TextArea
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              required
+            />
+          </FormBlockMessage>
+          <FormInput>
+            <Checkbox type="checkbox" />
+            <p>
+              Ao informar meus dados, eu concordo com a Política de
+              Privacidade e concordo em receber comunicações.
+            </p>
+          </FormInput>
+        </Form>
+        <Button type="submit">Receber atendimento</Button>
+        {success && (
+          <SuccessMessage>Mensagem enviada com sucesso!</SuccessMessage>
+        )}
+      </ContainerContent>
     </Container>
   );
 };
--- src/app/components/ContactForm/styles.ts
+++ src/app/components/ContactForm/styles.ts
@@ -1,49 +1,169 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
-  padding: 40px;
-  background-color: #f7f7f7;
-  text-align: center;
+  height: 898px;
+  padding: 64px 0 64px 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 64px;
 `;
 
-export const Form = styled.form`
+export const ContainerContent = styled.div`
+  width: 870px;
+  height: 898px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid red;
+`;
+
+export const TitleForm = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 15px;
-  width: 100%;
-  max-width: 600px;
-  margin: 0 auto;
+  justify-content: center;
+  width: 870px;
+  height: 126px;
+  gap: 24px;
+  color: #5D5D5D;
+
+  h2 {
+    font-size: 45px;
+    line-height: 54px;
+    font-weight: 500;
+    height: 54px;
+  }
+
+  p {
+    font-size: 20px;
+    line-height: 24px;
+    font-weight: 400;
+    height: 24px;
+    width: 860px;
+    text-align: center;
+  }
+
+`;
+
+export const Form = styled.form`
+  width: 870px;
+  height: 620px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+  border: 1px solid red;
+`;
+
+export const FormGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr); 
+  gap: 20px;
+  border: 1px solid red;
+`;
+
+export const FormBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 8px;
+  height: 110px;
+  border: 1px solid red;
+  width: 425px;
+`;
+
+export const FormBlockMessage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 8px;
+  height: 200px;
+  border: 1px solid red;
+`;
+
+export const FormInput = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 14px;
+  height: 44px;
+  border: 1px solid red;
+  width: 844px;
+  align-self: center;
+  p{
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 21.6px;
+    color: #5D5D5D;
+  }
+`;
+
+export const Label = styled.label`
+  font-size: 20px
+  font-weight: 700;
+  line-height: 24px;
+  color: #5D5D5D;
 `;
 
 export const Input = styled.input`
-  width: 100%;
+  width: 423px;
+  height: 56px;
   padding: 10px;
   font-size: 1rem;
-  border: 1px solid #ccc;
+  background-color: transparent;
+  border: 1px solid #C6B92D;
   border-radius: 5px;
+  outline: none;
+  color: black;
+`;
+
+export const Option = styled.select`
+  width: 423px;
+  height: 56px;
+  padding: 10px;
+  font-size: 1rem;
+  background-color: transparent;
+  border: 1px solid #C6B92D;
+  border-radius: 5px;
+  outline: none;
+  color: black;
 `;
 
 export const TextArea = styled.textarea`
   width: 100%;
+  height: 200px;
   padding: 10px;
   font-size: 1rem;
-  border: 1px solid #ccc;
+  background-color: transparent;
+  border: 1px solid #C6B92D;
   border-radius: 5px;
-  height: 150px;
+  outline: none;
+  color: black;
 `;
 
 export const Button = styled.button`
-  padding: 10px 20px;
-  font-size: 1rem;
-  color: #fff;
-  background-color: #7dba40;
+  width: 360px;
+  height: 64px;
+  border-radius: 50px;
+  padding: 20px 30px 20px 30px;
+  background-color: #C6B92D; 
   border: none;
-  border-radius: 5px;
+  color: white;
+  font-size: 20px;
+  font-weight: 700;
+  line-height: 24px;
   cursor: pointer;
-  &:hover {
-    background-color: #6ba933;
-  }
+
+`;
+
+export const Checkbox = styled.input`
+  width: 22px;
+  height: 22px;
+  border: 5px solid #C6B92D;
+  border-radius: 4px;
+  outline: none;
 `;
 
 export const SuccessMessage = styled.p`
