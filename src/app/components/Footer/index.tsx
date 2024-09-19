e061a78c784bc27df1e08b6b2b4da503be36ab72
MrGhostsnow <96500538+MrGhostsnow@users.noreply.github.com>
2024-09-19 18:52:00 -0300

    feat: footer

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -1,16 +1,52 @@
-import { Container, Copyright, SocialLinks } from "./styles";
+import Image from "next/image";
+import {
+  Container,
+  ContainerContent,
+  Company,
+  SAC,
+  SectionInfos,
+  SectionContact,
+  Copyright,
+  SocialLinks,
+} from "./styles";
+
+import logo from "../../assets/logo.png";
+import facebook from "../../assets/facebook.png";
+import instagram from "../../assets/instagram.png";
+import linkedin from "../../assets/linkedin.png";
 
 const Footer = () => {
   return (
     <Container>
-      <SocialLinks>
-        <a href="https://twitter.com">Twitter</a>
-        <a href="https://linkedin.com">LinkedIn</a>
-        <a href="https://github.com">GitHub</a>
-      </SocialLinks>
+      <ContainerContent>
+        <Company>
+          <Image src={logo} alt="Logo" width={190} height={55} />
+          <p>
+            A Olivas Digital é uma martech (marketing technology company), que
+            há mais de 10 anos ajuda empresas de todos os portes e segmentos a
+            impulsionarem suas vendas no digital.
+          </p>
+        </Company>
+        <SAC>
+          <h3>Fale conosco</h3>
+          <SectionInfos>
+            <SectionContact>
+              <p style={{ fontWeight: 700 }}>Atendimento</p>
+              <p>+55(11) 2391-6090</p>
+            </SectionContact>
+            <SectionContact>
+              <p style={{ fontWeight: 700 }}>Redes Sociais</p>
+              <SocialLinks>
+                <Image src={facebook} alt="Facebook" width={32} height={32} />
+                <Image src={instagram} alt="Instagram" width={32} height={32} />
+                <Image src={linkedin} alt="Linkedin" width={32} height={32} />
+              </SocialLinks>
+            </SectionContact>
+          </SectionInfos>
+        </SAC>
+      </ContainerContent>
       <Copyright>
-        &copy; {new Date().getFullYear()} olivas.digital. Todos os direitos
-        reservados.
+        <p>© 2014-2024 - Olivas Digital</p>
       </Copyright>
     </Container>
   );
--- src/app/components/Footer/styles.ts
+++ src/app/components/Footer/styles.ts
@@ -1,14 +1,87 @@
 import styled from 'styled-components';
 
 export const Container = styled.footer`
-  padding: 20px;
-  background-color: #333;
-  color: #fff;
-  text-align: center;
+  background-color: #E6E6E6;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  height: 227px;
 `;
 
+export const ContainerContent = styled.div`
+  height: 155px;
+  background-color: #E6E6E6;
+  display: flex;
+  width: 800px;
+  padding: 30px 0;
+  gap: 56px;
+`;
+
+export const Company = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 10px;
+  width: 417px;
+  height: 125px;
+
+  p {
+    font-size: 14px;
+    color: #7F7F7F;
+    font-weight: 400;
+    line-height: 16.8px;
+  }
+`;
+
+export const SAC = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 10px;
+
+   h3{
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 24px;
+    color: #5D5D5D;
+  }
+`;
+
+export const SectionInfos = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 35px;
+  width: 326.06px;
+  height: 59px;
+`;
+
+export const SectionContact = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 10px;
+  width: 147px;
+
+  p {
+    font-size: 16px;
+    color: #7F7F7F;
+    font-weight: 400;
+    line-height: 19.2px;
+  }
+ 
+`;
+
+
 export const SocialLinks = styled.div`
-  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 24px;
 
   a {
     color: #7dba40;
@@ -22,7 +95,19 @@ export const SocialLinks = styled.div`
   }
 `;
 
-export const Copyright = styled.p`
-  font-size: 0.9rem;
-  color: #aaa;
+export const Copyright = styled.div`
+  background-color: #B1B1B1;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 46px;
+
+  p {
+    font-size: 16px;
+    color: #FFFFFF;
+    font-weight: 700;
+    line-height: 19.2px;
+    text-align: center;
+  }
 `;
